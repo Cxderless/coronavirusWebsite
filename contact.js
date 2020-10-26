858f76c1f40e4f4a525876c12932e40f9eead194
Coderless <coderless@protonmail.com>
2020-10-26 23:24:14 +0000

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,30 @@
+function myFunction() {
+    var x = document.getElementById("navigation");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+}
+
+function hideFirstText () {
+    var x = document.getElementById("first-section-list");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        document.getElementById("first-section-button").innerHTML = "Hide List";
+    } else {
+        x.style.display = "none";
+        document.getElementById("first-section-button").innerHTML = "Reveal List";
+    }
+}
+
+function hideSecondText () {
+    var x = document.getElementById("second-section-list");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        document.getElementById("second-section-button").innerHTML = "Hide List";
+    } else {
+        x.style.display = "none";
+        document.getElementById("second-section-button").innerHTML = "Reveal List";
+    }
+}
