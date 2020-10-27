261c18126f0403791a80c5f08e37e9329946e0be
Coderless <coderless@protonmail.com>
2020-10-27 13:19:18 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function myFunction() {
+    var x = document.getElementById("navigation");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+}
+
+function hideFirstText () {
+    var x = document.getElementById("first-section-text");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        document.getElementById("first-section-button").innerHTML = "Hide Text";
+    } else {
+        x.style.display = "none";
+        document.getElementById("first-section-button").innerHTML = "Reveal Text";
+    }
+}
+
+function hideSecondText () {
+    var x = document.getElementById("second-section-text");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        document.getElementById("second-section-button").innerHTML = "Hide Text";
+    } else {
+        x.style.display = "none";
+        document.getElementById("second-section-button").innerHTML = "Reveal Text";
+    }
+}
+
+function hideThirdText () {
+    var x = document.getElementById("third-section-list");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        document.getElementById("third-section-button").innerHTML = "Hide List";
+    } else {
+        x.style.display = "none";
+        document.getElementById("third-section-button").innerHTML = "Reveal List";
+    }
+}
